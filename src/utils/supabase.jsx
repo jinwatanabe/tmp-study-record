f175d9d6c8cf8dbbc472774ab95f2bc32e9a2d3b
acu8 <ewatanabe786@gmail.com>
2024-07-06 20:45:04 +0900

    test

--- src/utils/supabase.jsx
+++ src/utils/supabase.jsx
@@ -2,8 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  // import.meta.env.VITE_SUPABASE_URL,
-  // import.meta.env.VITE_SUPABASE_ANON_KEY
-  "https://znpznaaskoqtznmttcao.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpucHpuYWFza29xdHpubXR0Y2FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5NDE5MTAsImV4cCI6MjAzNTUxNzkxMH0.Kw31mHCkQxYV71J9_m4Vp_dCoFLIJQPre3QRlW9N3HI"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
