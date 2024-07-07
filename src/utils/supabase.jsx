960aab48f187efd41be53aba530c13fe5003b7d1
jinwatanabe <jin.watanabe.6g@gmail.com>
2024-07-07 21:38:58 +0900

    test

--- src/utils/supabase.jsx
+++ src/utils/supabase.jsx
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
+
+console.log(import.meta.env.VITE_SUPABASE_URL)
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
